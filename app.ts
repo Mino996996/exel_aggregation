ace03c3ba10dc05a082f088d63ae6197383acd5e
minoru akahori <minoruaka@gmail.com>
2021-10-28 14:31:25 +0900

    refactor(app): refactoring

--- app.ts
+++ app.ts
@@ -1,19 +1,14 @@
 const XLSX = require('xlsx');
+const Aggregate = require('./Aggregate');
 
 const excelData = XLSX.readFile('./assets/data_sample.xlsx');
 
 let sheet = excelData.Sheets[excelData.SheetNames[0]];
-
-// console.log(sheet['!ref']);
-
 sheet['!ref'] = 'E1:E108';
-
 const sheet_json = XLSX.utils.sheet_to_json(sheet);
+const key = sheet['E1'].v;
 
-console.log(sheet_json);
+const aggregate = new Aggregate(sheet_json, key);
 
-// let count = 0;
-// for(let sell of sheet_json) {
-//   console.log(`${count} :${sell}`);
-//   count +=1;
-// }
+console.log(aggregate.allCounts());
+
